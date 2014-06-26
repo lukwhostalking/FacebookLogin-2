ba1c2577a1ab1ff2ae131ff2797b6fe52e514fb9
ANUPRIYA AGRAWAL <anupriyaagrawal@ANUPRIYAs-MacBook-Air.local>
2014-06-25 20:56:57 -0400

    Initial Commit

---
+++ FacebookLogin/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FacebookLogin
+//
+//  Created by ANUPRIYA AGRAWAL on 6/25/14.
+//  Copyright (c) 2014 IshanBhalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
